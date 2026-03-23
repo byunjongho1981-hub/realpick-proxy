dcd43a1d9be0d94d25bbf956c96daf0a0543870b
byunjongho1981-hub <byunjongho1981@gmail.com>
2026-03-23 16:33:10 +0900

    Update fetch-url.js

--- api/fetch-url.js
+++ api/fetch-url.js
@@ -3,7 +3,7 @@
 
 const crypto = require('crypto');
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
