df788413dab9c22be4e13f7c6b00bc9d9772a225
byunjongho1981-hub <byunjongho1981@gmail.com>
2026-03-24 17:09:51 +0900

    Delete api/google-hot.js

--- api/google-hot.js
+++
@@ -1,4 +0,0 @@
-module.exports = function(req, res){
-  res.setHeader('Access-Control-Allow-Origin','*');
-  res.status(200).json({items:[], total:0, message:'준비 중', updatedAt:new Date().toISOString()});
-};
